e92402f6e73bfd806e850e50c192b08ce07bf60b
RenataCasoni <87589067+RenataCasoni@users.noreply.github.com>
2024-11-06 21:54:40 -0300

    commit2

--- main.py
+++ main.py
@@ -3,40 +3,41 @@ def importar_dados():
 
     with open("alocacoes.txt", "r") as file:
         for line in file:
-            nome_funcionario, nome_projeto = line.strip().split(';')
-            alocacoes[nome_funcionario] = nome_funcionario
-            alocacoes[nome_projeto] = nome_projeto
+            if(not line.startswith('NomeFuncionario')):
+             nome_funcionario, nome_projeto = line.strip().split(';')
+             alocacoes[nome_funcionario] = nome_funcionario
+             alocacoes[nome_funcionario] = nome_projeto
 
     projetos = {}
     with open("projetos.txt", "r") as file:
-        texto = []
         for line in file:
-            if line[0:2] != "##":
-                texto += [linha.strip().split(" ")]
-            nome, departamento_responsavel = line.strip().split(';')
-            projetos[nome] = nome
-            projetos[departamento_responsavel] = departamento_responsavel
+            if(not line.startswith('Nome')):
+             nome, departamento_responsavel = line.strip().split(';')
+             projetos[nome] = nome
+             projetos[nome] = departamento_responsavel
 
     departamentos = {}
     with open("departamentos.txt", "r") as file:
         for line in file:
-            nome, qtd_funcionarios = line.strip().split(';')
-            departamentos[nome] = nome
-            departamentos[qtd_funcionarios] = qtd_funcionarios
+            if(not line.startswith('Nome')):
+             nomedep, qtd_funcionarios = line.strip().split(';')
+             departamentos[nomedep] = nomedep
+             departamentos[nomedep] = qtd_funcionarios
 
     funcionarios = {}
     with open("funcionarios.txt", "r") as file:
         for line in file:
-            nome, departamentos, AnosExperiencia = line.strip().split(';')
-            funcionarios[nome] = nome
-            funcionarios[qtd_funcionarios] = qtd_funcionarios
-            funcionarios[nome] = AnosExperiencia
+            if(not line.startswith('Nome')):
+             nome, departamentos, AnosExperiencia = line.strip().split(';')
+             funcionarios[nome] = nome
+             funcionarios[nome] = qtd_funcionarios
+             funcionarios[nome] = AnosExperiencia
 
     return alocacoes, projetos, departamentos, funcionarios
 
 alocacoes, projetos, departamentos, funcionarios = importar_dados()
 
 print("Projetos:", projetos)
-#print("Alocações:", alocacoes)
-#print("Alocações:", departamentos)
-#print("Alocações:", funcionarios)
+print("Alocações:", alocacoes)
+print("Departamentos:", departamentos)
+print("Funcionarios:", funcionarios)
